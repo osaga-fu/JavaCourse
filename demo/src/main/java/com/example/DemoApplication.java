e7186fdbc78945ee596bf3838fd7fdd9872f37e4
olasanch <olasanch@LES011580.corp.capgemini.com>
2024-06-18 19:33:45 +0200

    Profiles, application properties and strict null

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.ioc.Entorno;
+import com.example.ioc.Rango;
 import com.example.ioc.Saluda;
 
 @SpringBootApplication
@@ -13,14 +15,28 @@ public class DemoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	@Autowired
-	Saluda saluda;
 
+	@Autowired
+//	@Qualifier("es")
+	Saluda saluda;
+	@Autowired
+//	@Qualifier("en")
+	Saluda saluda2;
+	@Autowired
+	Entorno entorno;
+	@Autowired
+	private Rango rango;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("Aplicación arrancada");
+		System.err.println("Aplicación arrancada...");
+		System.out.println(saluda.getContador());
 		saluda.saluda("Mundo");
+		saluda2.saluda("Mundo");
+		System.out.println(saluda.getContador());
+		System.out.println(saluda2.getContador());
+		System.out.println(entorno.getContador());
+		System.out.println(rango.getMin() + " -> " + rango.getMax());
 	}
 
 }
