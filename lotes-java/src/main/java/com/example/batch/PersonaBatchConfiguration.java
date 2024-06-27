f95c66f1a7e85bde79401d6a0cd8af300eeb689a
olasanch <olasanch@LES011580.corp.capgemini.com>
2024-06-27 13:03:08 +0200

    Create personaDBItemWriter

--- lotes-java/src/main/java/com/example/batch/PersonaBatchConfiguration.java
+++ lotes-java/src/main/java/com/example/batch/PersonaBatchConfiguration.java
@@ -1,14 +1,22 @@
 package com.example.batch;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.example.models.Persona;
 import com.example.models.PersonaDTO;
 
 @Configuration
@@ -29,5 +37,18 @@ public class PersonaBatchConfiguration {
 						setTargetType(PersonaDTO.class);
 					}})
 				.build();
+	}
+	
+	@Autowired
+	public PersonaItemProcessor personaItemProcessor;
+	
+	@Bean
+	@DependsOnDatabaseInitialization
+	public JdbcBatchItemWriter<Persona> personaDBItemWriter(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Persona>()
+					.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+					.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)")
+					.dataSource(dataSource)
+					.build();
 	}
 }
