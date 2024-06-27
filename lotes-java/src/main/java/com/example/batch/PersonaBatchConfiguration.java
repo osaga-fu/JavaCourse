22748e1267167f2a7e5ec2186fc55f91ec47a807
olasanch <olasanch@LES011580.corp.capgemini.com>
2024-06-27 13:46:27 +0200

    Create Step from DB to CSV

--- lotes-java/src/main/java/com/example/batch/PersonaBatchConfiguration.java
+++ lotes-java/src/main/java/com/example/batch/PersonaBatchConfiguration.java
@@ -10,16 +10,23 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.models.Persona;
@@ -32,6 +39,8 @@ public class PersonaBatchConfiguration {
 	
 	@Autowired
 	PlatformTransactionManager transactionManager;
+	
+	// CSV to DB
 	
 	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
 		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
@@ -68,12 +77,66 @@ public class PersonaBatchConfiguration {
 				.build();
 	}
 	
+	// Just 1 Step Job
+	
+//	@Bean
+//	public Job personasJob(PersonaJobListener listener, Step importCSV2DBStep1) {
+//		return new JobBuilder("personasJob", jobRepository)
+//				.incrementer(new RunIdIncrementer())
+//				.listener(listener)
+//				.start(importCSV2DBStep1)
+//				.build();
+//	}
+	
+	// BD to CSV
+	
+	
 	@Bean
-	public Job personasJob(PersonaJobListener listener, Step importCSV2DBStep1) {
+	JdbcCursorItemReader<Persona> personaDBItemReader(DataSource dataSource) {
+		return new JdbcCursorItemReaderBuilder<Persona>().name("personaDBItemReader")
+				.sql("SELECT id, nombre, correo, ip FROM personas").dataSource(dataSource)
+				.rowMapper(new BeanPropertyRowMapper<>(Persona.class))
+				.build();
+	}
+	
+	@Bean
+	public FlatFileItemWriter<Persona> personaCSVItemWriter() {
+		return new FlatFileItemWriterBuilder<Persona>().name("personaCSVItemWriter")
+				.resource(new FileSystemResource("output/outputData.csv"))
+				.lineAggregator(new DelimitedLineAggregator<Persona>() {
+					{
+						setDelimiter(",");
+						setFieldExtractor(new BeanWrapperFieldExtractor<Persona>() {
+							{
+								setNames(new String[] { "id", "nombre", "correo", "ip" });
+							}
+						});
+					}
+				}).build();
+	}
+	
+	@Bean
+	public Step exportDB2CSVStep(JdbcCursorItemReader<Persona> personaDBItemReader) {
+		return new StepBuilder("exportDB2CSVStep", jobRepository)
+				.<Persona, Persona>chunk(100, transactionManager)
+				.reader(personaDBItemReader)
+				.writer(personaCSVItemWriter())
+				.build();
+	}
+	
+	
+	// Job
+
+	@Bean
+	public Job personasJob(PersonaJobListener listener, Step importCSV2DBStep1,
+			Step exportDB2CSVStep) {
 		return new JobBuilder("personasJob", jobRepository)
 				.incrementer(new RunIdIncrementer())
 				.listener(listener)
 				.start(importCSV2DBStep1)
+				.next(exportDB2CSVStep)
 				.build();
 	}
+	
+
 }
--- lotes-java/src/main/java/com/example/models/Persona.java
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -2,9 +2,11 @@ package com.example.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data 
 @AllArgsConstructor
+@NoArgsConstructor
 public class Persona {
 	private long id;
 	private String nombre;
