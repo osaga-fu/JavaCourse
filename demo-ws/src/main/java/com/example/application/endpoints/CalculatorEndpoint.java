c6407c0ee4bd4a772c0f94a4751161487c8116bb
olasanch <olasanch@LES011580.corp.capgemini.com>
2024-07-01 08:54:01 +0200

    Refactor Calculator endpoints

--- demo-ws/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
+++ demo-ws/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
@@ -1,9 +1,13 @@
 package com.example.application.endpoints;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapFaultException;
 
 import com.example.webservices.schemas.calculator.AddRequest;
 import com.example.webservices.schemas.calculator.AddResponse;
@@ -30,7 +34,7 @@ public class CalculatorEndpoint {
 	@ResponsePayload
 	public SubResponse sub(@RequestPayload SubRequest request) {
 		var result = new SubResponse();
-		result.setSubResult(request.getOp1() - request.getOp2());
+		result.setSubResult(round(request.getOp1() - request.getOp2()));
 		return result;
 	}
 	
@@ -38,16 +42,22 @@ public class CalculatorEndpoint {
 	@ResponsePayload
 	public MulResponse mul(@RequestPayload MulRequest request) {
 		var result = new MulResponse();
-		result.setMulResult(request.getOp1() * request.getOp2());
+		result.setMulResult(round(request.getOp1() * request.getOp2()));
 		return result;
 	}
 	
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "divRequest")
 	@ResponsePayload
 	public DivResponse div(@RequestPayload DivRequest request) {
+		if(request.getOp2() == 0)
+			throw new SoapFaultException("Division by 0", new ArithmeticException("/ by zero")); //IllegalArgumentException();
 		var result = new DivResponse();
-		result.setDivResult(request.getOp1() / request.getOp2());
+		result.setDivResult(round(request.getOp1() / request.getOp2()));
 		return result;
+	}
+	
+	private double round(double o) {
+		return (new BigDecimal(o)).setScale(16, RoundingMode.HALF_UP).doubleValue();
 	}
 	
 }
